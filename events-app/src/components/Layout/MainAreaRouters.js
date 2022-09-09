e3ea3483a1765904f9d01405798e86ac1af074e8
Robin-Hood-zjw <jiawenz@umich.edu>
2022-09-08 22:53:39 -0400

    make react router works

--- events-app/src/components/Layout/MainAreaRouters.js
+++ events-app/src/components/Layout/MainAreaRouters.js
@@ -2,7 +2,7 @@ import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 import Layout from "../DefaultLayout";
-import EventsPage from "./MainArea/EventPage/EventsPage";
+import EventsPage from "./MainArea/EventsPage/EventsPage";
 import EventPage from "./MainArea/EventPage/EventPage";
 import AttendeesPage from "./MainArea/AttendeesPage/AttendeesPage";
 import CommentsPage from "./MainArea/CommentsPage/CommentsPage";
@@ -13,17 +13,13 @@ const MainArea = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/events" element={<Layout />}>
-          <Route index element={<EventsPage />}>
-            <Route path=":id" element={<EventPage />}>
-              <Route path=":id/attendees" element={<AttendeesPage />} />
-              <Route path=":id/comments" element={<CommentsPage />} />
-            </Route>
-          </Route>
-        </Route>
-
-        <Route path="/create-event" element={<CreateEventPage />} />
-        <Route path="/profile" element={<ProfilePage />} />
+        <Route path="/" element={<Layout />} />
+        <Route index element={<EventsPage />} />
+        <Route path="/events/:id" element={<EventPage />} />
+        <Route path="/events/:id/attendees" element={<AttendeesPage />} />
+        <Route path="/events/:id/comments" element={<CommentsPage />} />
+        <Route path="create-event" element={<CreateEventPage />} />
+        <Route path="profile" element={<ProfilePage />} />
       </Routes>
     </BrowserRouter>
   );
