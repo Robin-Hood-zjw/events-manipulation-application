af5e3f2f591eac47a4de5269167afeb95239bd9c
Robin-Hood-zjw <jiawenz@umich.edu>
2022-09-25 16:16:35 -0400

    some import updates

--- events-app/src/components/Layout/MainAreaRouters.js
+++ events-app/src/components/Layout/MainAreaRouters.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 
 import Layout from "../DefaultLayout";
 import EventsPage from "./MainArea/EventsPage/EventsPage";
