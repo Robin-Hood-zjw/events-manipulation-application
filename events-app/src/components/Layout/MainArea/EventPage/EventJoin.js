fe602c4df9d06be8b127d93fa12f18bd6545447f
Robin-Hood-zjw <jiawenz@umich.edu>
2022-09-26 14:04:25 -0400

    tweak EventPage methods

--- events-app/src/components/Layout/MainArea/EventPage/EventJoin.js
+++ events-app/src/components/Layout/MainArea/EventPage/EventJoin.js
@@ -1,44 +1,48 @@
-import React, { useState } from 'react';
-import { Button, Modal } from 'antd';
+import React, { useState } from "react";
+import { Button, Modal } from "antd";
 
 const { confirm } = Modal;
 
 const EventJoin = (props) => {
-    const [isModalVisible, setModalVisible] = useState(false);
+  const [isModalVisible, setModalVisible] = useState(false);
 
-    onJoin = () => {
-        confirm({
-            title: 'Do you want to join?',
-            centered: true,
-        
-            okText: 'Join',
-            onOk() {
-              console.log('OK');
-            },
-          });
-    }
+  const onJoin = () => {
+    confirm({
+      title: "Do you want to join?",
+      centered: true,
 
-    onRate = () => setModalVisible(!isModalVisible);
+      okText: "Join",
+      onOk() {
+        console.log("OK");
+      },
+    });
+  };
 
-    return (
-        <div>
-            <section>
-                <p></p>
-                <p></p>
-                <p>Hosted by {}</p>
-            </section>
+  const onRate = () => setModalVisible(!isModalVisible);
 
-            <section>
-                <Button type='primary' onClick={onJoin}>JOIN THIS EVENT</Button>
-                <Button type='primary' onClick={onRate}>RATE THIS EVENT</Button>
-            </section>
+  return (
+    <div>
+      <section>
+        <p></p>
+        <p></p>
+        <p>Hosted by {}</p>
+      </section>
 
-            {/* Rate Modal */}
-            <Modal>
-                <Button type='primary'>Submit</Button>
-            </Modal>
-        </div>
-    );
-}
+      <section>
+        <Button type="primary" onClick={onJoin}>
+          JOIN THIS EVENT
+        </Button>
+        <Button type="primary" onClick={onRate}>
+          RATE THIS EVENT
+        </Button>
+      </section>
+
+      {/* Rate Modal */}
+      <Modal>
+        <Button type="primary">Submit</Button>
+      </Modal>
+    </div>
+  );
+};
 
 export default EventJoin;
